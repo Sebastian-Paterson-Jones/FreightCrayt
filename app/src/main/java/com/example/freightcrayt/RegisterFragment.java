a6b20f30abb67e067a85cdbaa5ecbb0fb9b95e43
sebastian paterson-jones <sebastian.boo.pj@gmail.com>
2022-05-06 12:47:24 +0200

    :sparkles: Login and register fragment navigation support

---
+++ app/src/main/java/com/example/freightcrayt/RegisterFragment.java
@@ -0,0 +1,31 @@
+package com.example.freightcrayt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    public RegisterFragment() {
+    }
+
+    public static RegisterFragment newInstance() {
+        RegisterFragment fragment = new RegisterFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
