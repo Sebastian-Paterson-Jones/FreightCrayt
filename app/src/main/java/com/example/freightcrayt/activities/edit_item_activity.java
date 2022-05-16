4cd7e7f2333e48da83940a0668d0eb0631349934
Jared Wild <jareddavidwild4@gmail.com>
2022-05-16 12:37:46 +0200

    :sparkles: Edit Item Activity GUI
    just copied and pasted the new item gui, switched some labels. No back-end just yet

---
+++ app/src/main/java/com/example/freightcrayt/activities/edit_item_activity.java
@@ -0,0 +1,16 @@
+package com.example.freightcrayt.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.freightcrayt.R;
+
+public class edit_item_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+    }
+}
